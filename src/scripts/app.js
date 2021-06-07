966190b7612ccceacebc56edbda18792f74ec8d7
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-07 18:06:54 +0200

    added event to update score on page load

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,19 @@
 import { getQuery, getAllQuery } from './utils'
 import { handleRules } from './rulesHandler'
+import { handleGame } from './gameHandler'
+import { getScore, updateScoreDOM } from './scoreHandler'
 
 getAllQuery('.rules-button').forEach(button =>
   button.addEventListener('click', handleRules)
 )
+
+getAllQuery('.game__picker-outside').forEach(picker =>
+  picker.addEventListener('click', handleGame)
+)
+
+window.addEventListener('load', () => {
+  const score = getScore()
+  console.log('hello')
+
+  updateScoreDOM(score)
+})
